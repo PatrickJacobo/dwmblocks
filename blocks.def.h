48b998954bafa409d963d825c5d9ee5294289925
JacobsLaptop <leepatrick338@gmail.com>
2024-10-24 16:58:35 -0600

    Changed a couple of functions the Makefile to erase config.h automatically

--- blocks.def.h
+++ blocks.def.h
@@ -2,6 +2,10 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+	{"CPU:", "top -bn1 | grep 'Cpu(s)' | awk '{print $2 + $4}'",	30,		0},
+	{"Bat:", "upower -i $(upower -e | grep BAT) | grep -E 'percentage' | awk '{print $2}'",	30,		0},
+
+
 
 	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
 };
